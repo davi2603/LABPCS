2f976cb6acc3d90f7825bc2838e7fd88984054d7
davi2603 <davi2603@gmail.com>
2021-10-06 17:31:51 -0300

    Add files via upload

---
+++ JDPCSDEPLOY.ps1
@@ -0,0 +1,44 @@
+﻿
+
+$ARQ=Get-ChildItem -Path C:\JDPCS\Atualizacoes  -Recurse -ErrorAction SilentlyContinue -Filter *.dll |
+  Where-Object { $_.Extension -eq '.dll' }
+  echo $ARQ.name
+foreach( $ARQUIVOS in $ARQ ) {
+  if ( $ARQUIVOS.name 	-like '*cabineweb*')
+  {
+  Copy-Item -Path $ARQUIVOS.FullName  -Destination "C:\JDPCS\Modulo_WEBSite\DLL\" -Recurse -Force  
+}
+
+ if ( $ARQUIVOS.name 	-like '*transmissor*')
+  {
+  Copy-Item -Path $ARQUIVOS.FullName  -Destination "C:\JDPCS\Servicos" -Recurse -Force  
+}
+
+ if ( $ARQUIVOS.name 	-like '*receptor*')
+  {
+  Copy-Item -Path $ARQUIVOS.FullName  -Destination "C:\JDPCS\Servicos" -Recurse -Force  
+}
+
+ if ( $ARQUIVOS.name 	-like '*portabilidade*')
+  {
+  Copy-Item -Path $ARQUIVOS.FullName  -Destination "C:\JDPCS\WS\DLL" -Recurse -Force  
+}
+
+
+
+}
+
+$DIR=Get-ChildItem -Path C:\JDPCS\Atualizacoes  -Recurse -ErrorAction   SilentlyContinue -Directory -Filter "DLL" 
+if ($DIR) {
+   $DIRPATH=$DIR.FullName
+   echo $DIRPATH
+   Copy-Item -Path $DIRPATH\*  -Destination "C:\JDPCS\Modulo_WEBSite\DLL\" -Recurse -Force 
+  }
+
+
+  $DIRHOME=Get-ChildItem -Path C:\JDPCS\Atualizacoes  -Recurse -ErrorAction   SilentlyContinue -Directory -Filter "home" 
+if ($DIRHOME) {
+   $DIRHOMEPATH=$DIRHOME.FullName
+   echo $DIRHOMEPATH
+   Copy-Item -Path $DIRHOMEPATH\*  -Destination "C:\JDPCS\Modulo_WEBSite\Home\" -Recurse -Force 
+  }
---
+++ Sem_título4.ps1
@@ -0,0 +1,23 @@
+﻿add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+
+#$url= $1
+#echo $url
+
+$param1=$args[0]
+write-host $param1 
+
+
+Invoke-WebRequest  -Uri $param1  -OutFile 'C:\JDPCS\teste.zip'
+
